a3f09903be4d4d4e0d4aabe67db11d36753c40cc
Prasun Timalsina <timalsinaprasun61@gmail.com>
2025-03-18 21:07:39 +0545

    Add a calendar

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ const displayEmojiConEl = document.querySelector(".show-mood-container .emoji");
 const displayNoteConEl = document.querySelector(".show-mood-container .note");
 const displayDateConEl = document.querySelector(".show-mood-container .date");
 const inputContainerEl = document.querySelector(".input-container");
+///Calendar elements
+const daysTag = document.querySelector(".days"),
+  currentDate = document.querySelector(".current-date"),
+  prevNextIcon = document.querySelectorAll(".icons span");
 /////variable/////
 const state = {
   moods: [
@@ -76,6 +80,11 @@ const months = [
   "November",
   "December",
 ];
+// getting new date, current year and month
+let date = new Date(),
+  currYear = date.getFullYear(),
+  currMonth = date.getMonth(),
+  currDate = date.getDate();
 
 /////functions/////
 //adding mood
@@ -108,7 +117,12 @@ const displayData = (date) => {
   //get data based on date
   console.log(date);
   const mood = getMood(date);
-  console.log(mood);
+
+  //guard clause
+  if (!mood) {
+    alert("Invalid date or your haven't fill the data for the chosen date😗");
+    return;
+  }
   //tooggle input and display
   DisplaydataContainerEl.style.display = "flex";
   inputContainerEl.style.display = "none";
@@ -122,18 +136,73 @@ const displayData = (date) => {
   const year = new Date(date).getFullYear();
   displayDateConEl.textContent = `${day}, ${month} ${dateNum}, ${year}`;
 };
-displayData("2025-03-12");
+/* displayData("2025-03-12"); */
+
+//display input
 const displayInputs = () => {
   DisplaydataContainerEl.style.display = "none";
   inputContainerEl.style.display = "flex";
 };
+
+////calender related functions
+//handle click event in calendar elements
+const handleClickOnDate = (target) => {
+  console.log(target);
+  const dateEl = target;
+  const date = dateEl.dataset.date;
+  displayData(date);
+};
+//rendering calender
+const renderCalendar = () => {
+  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+  let liTag = "";
+  for (let i = firstDayofMonth; i > 0; i--) {
+    // creating li of previous month last days
+    liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+  }
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    // creating li of all days of current month
+    // adding active class to li if the current day, month, and year matched
+    let isToday =
+      i === date.getDate() &&
+      currMonth === new Date().getMonth() &&
+      currYear === new Date().getFullYear()
+        ? "active"
+        : "";
+
+    liTag += `<li class="${isToday} date-ele " data-date = "${currYear}-${
+      Number(currMonth) + 1 > 9
+        ? `${date.getMonth() + 1}`
+        : `0${date.getMonth() + 1}`
+    }-${i}">${i}</li>`;
+  }
+  for (let i = lastDayofMonth; i < 6; i++) {
+    // creating li of next month first days
+    liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+  }
+  currentDate.innerText = `${months[Number(currMonth)]} ${currYear}`; // passing current mon and yr as currentDate text
+  daysTag.innerHTML = liTag;
+  //selecting all date elements
+  const dateElements = document.querySelectorAll(".date-ele");
+  //adding events to date
+  dateElements.forEach((dateEl) => {
+    dateEl.addEventListener("click", () => {
+      handleClickOnDate(dateEl);
+    });
+  });
+};
+renderCalendar();
 
 ////helper functions
 //date formater to store
 function formatDate(date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+  const day = String(date.getDate());
+  /* const day = String(date.getDate()).padStart(2, "0"); */
 
   return `${year}-${month}-${day}`;
 }
@@ -157,4 +226,24 @@ addMoodBtn.addEventListener("click", () => {
   //clearing inputs
   selectedMoodBoxEl.textContent = "";
   noteInputEl.value = "";
+});
+
+//event to next and prev in calendar
+prevNextIcon.forEach((icon) => {
+  // getting prev and next icons
+  icon.addEventListener("click", () => {
+    // adding click event on both icons
+    // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+    currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+    if (currMonth < 0 || currMonth > 11) {
+      // if current month is less than 0 or greater than 11
+      // creating a new date of current year & month and pass it as date value
+      date = new Date(currYear, currMonth, new Date().getDate());
+      currYear = date.getFullYear(); // updating current year with new date year
+      currMonth = date.getMonth(); // updating current month with new date month
+    } else {
+      date = new Date(); // pass the current date as date value
+    }
+    renderCalendar(); // calling renderCalendar function
+  });
 });
